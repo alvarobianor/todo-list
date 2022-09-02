034719184a30b34e5ec9e0755c146dc5a1b3f55e
Álvaro Medeiros <alvaro.medeiros@hp.com>
2022-09-02 20:32:54 -0300

    refatoring component to show empty infos and the content

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <div className={styles.wrapper}>
         <FindBar />
         <Board />
-        <InfosEmptyList />
+        <InfosEmptyList></InfosEmptyList>
       </div>
     </main>
   );
--- src/components/InfosEmptyList/index.tsx
+++ src/components/InfosEmptyList/index.tsx
@@ -1,18 +1,29 @@
 import styles from "./styles.module.css";
 import { ClipboardText } from "phosphor-react";
-export function InfosEmptyList() {
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export function InfosEmptyList({ children }: Props) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <ClipboardText size={56} />
-        <section className={styles.section}>
-          <strong className={styles.title}>
-            Você ainda não tem tarefas cadastradas
-          </strong>
-          <p className={styles.subject}>
-            Crie tarefas e organize seus itens a fazer
-          </p>
-        </section>
+        {children ? (
+          children
+        ) : (
+          <div className={styles.box}>
+            <ClipboardText size={56} />
+            <section className={styles.section}>
+              <strong className={styles.title}>
+                Você ainda não tem tarefas cadastradas
+              </strong>
+              <p className={styles.subject}>
+                Crie tarefas e organize seus itens a fazer
+              </p>
+            </section>
+          </div>
+        )}
       </div>
     </div>
   );
