8368cf16dae0d1b989da80c5c36ea2033a65f2d5
Álvaro Medeiros <alvaro.medeiros@hp.com>
2022-09-05 17:51:39 -0300

    Update and finish the interaction of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import { Board } from "./components/Board";
 import { FindBar } from "./components/FindBar";
 import { Header } from "./components/Header";
 import { ContentInfos } from "./components/ContentInfos";
 import { Task } from "./components/Task";
+import { KEY } from "./Utils/constants";
+
+export type TaskType = {
+  id: string;
+  isDone: boolean;
+  description: string;
+};
 
 function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem(KEY) as string) as TaskType[]);
+  }, []);
+
+  function handleToggleStatusTask(id: string) {
+    const taskIndex = tasks.findIndex((item) => item.id === id);
+    const updatedTasks = [...tasks];
+
+    updatedTasks[taskIndex].isDone = !updatedTasks[taskIndex].isDone;
+
+    localStorage.setItem(KEY, JSON.stringify(updatedTasks));
+    setTasks(updatedTasks);
+  }
+
+  function handleDeleteTask(id: string) {
+    const updatedTasks = tasks.filter((item) => item.id !== id);
+
+    localStorage.setItem(KEY, JSON.stringify(updatedTasks));
+    setTasks(updatedTasks);
+  }
+
   return (
     <main className={styles.main}>
       <Header />
@@ -14,12 +44,23 @@ function App() {
         <FindBar />
         <Board />
         <ContentInfos>
+          {tasks?.map((item) => {
+            return (
+              <Task
+                key={item.id}
+                allTasks={tasks}
+                task={item}
+                onToggleStatusTask={handleToggleStatusTask}
+                onDeleteTask={handleDeleteTask}
+              />
+            );
+          })}
+          {/* <Task />
           <Task />
           <Task />
           <Task />
           <Task />
-          <Task />
-          <Task />
+          <Task /> */}
         </ContentInfos>
       </div>
     </main>
--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -3,32 +3,42 @@ import { BiCircle } from "react-icons/bi";
 import { math } from "polished";
 import { FaCheckCircle } from "react-icons/fa";
 import { FiTrash2 } from "react-icons/fi";
+import { TaskType } from "../../App";
 
-export function Task() {
-  const random = Number(Math.random()) * 10 > 5 ? true : false;
+type Props = {
+  task: TaskType;
+  allTasks: TaskType[];
+  onToggleStatusTask: (id: string) => void;
+  onDeleteTask: (id: string) => void;
+};
+
+export function Task({
+  task: { id, description, isDone },
+  onToggleStatusTask,
+  onDeleteTask,
+}: Props) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <button className={styles.containerIconTask}>
-          {/*  TODO remove this */}
-          {random ? (
+        <button
+          className={styles.containerIconTask}
+          onClick={() => onToggleStatusTask(id)}
+        >
+          {isDone ? (
             <BiCircle size={24} className={styles.iconCircle} />
           ) : (
             <FaCheckCircle size={24} className={styles.iconCheckCircle} />
           )}
         </button>
         <div className={styles.descriptionTask}>
-          <p className={random ? styles.textToDo : styles.textDone}>
-            {random
-              ? `Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Officiis, voluptatibus nesciunt perspiciatis debitis error
-            consequatur quibusdam rerum tenetur, non repellat aliquam vel,
-            perferendis earum numquam accusamus exercitationem corrupti
-            assumenda quaerat.`
-              : "alguma coisa"}
+          <p className={isDone ? styles.textToDo : styles.textDone}>
+            {description}
           </p>
         </div>
-        <button className={styles.iconDeleteTask}>
+        <button
+          className={styles.iconDeleteTask}
+          onClick={() => onDeleteTask(id)}
+        >
           <FiTrash2 size={24} />
         </button>
       </div>
